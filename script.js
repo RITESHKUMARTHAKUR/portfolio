f4914f6231714768c291c376e0f3dcd5d7cb9d91
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2021-08-29 11:32:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+    const btn1 = document.querySelector(".btn1");
+    const des1 = document.querySelector(".des1");
+    const bhide = document.querySelector(".hide1");
+    function view1 (){
+        des1.style.display = "block";
+        des1.style.opacity = 1;
+        btn1.style.opacity = 0; 
+        bhide.style.opacity = 1;
+        bhide.style.display = "block";
+    }
+    function hide1 () {
+        des1.style.display = "none";
+        des1.style.opacity = 0;
+        btn1.style.opacity = 1;
+        bhide.style.opacity = 0;
+        bhide.style.display = "none";
+    }
+
+    const btn2 = document.querySelector(".btn2");
+    const des2 = document.querySelector(".des2");
+    const bhide2 = document.querySelector(".hide2");
+    function view2 (){
+        des2.style.display = "block";
+        des2.style.opacity = 1;
+        btn2.style.opacity = 0;        
+        bhide2.style.opacity = 1;
+        bhide2.style.display = "block";
+    }
+    function hide2 () {
+            des2.style.display = "none";
+            des2.style.opacity = 0;
+            btn2.style.opacity = 1;
+            bhide2.style.opacity = 0;
+            bhide2.style.display = "none";
+    }
+
+    const btn3 = document.querySelector(".btn3");
+    const des3 = document.querySelector(".des3");
+    const bhide3 = document.querySelector(".hide3");
+    function view3 (){
+        des3.style.display = "block";
+        des3.style.opacity = 1;
+        btn3.style.opacity = 0;       
+        bhide3.style.opacity = 1;
+        bhide3.style.display = "block";
+    }
+    function hide3 () {
+            des3.style.display = "none";
+            des3.style.opacity = 0;
+            btn3.style.opacity = 1;
+            bhide3.style.opacity = 0;
+            bhide3.style.display = "none";
+    }
+    const btn4 = document.querySelector(".btn4");
+    const des4 = document.querySelector(".des4");
+    const bhide4 = document.querySelector(".hide4");
+    function view4 (){
+        des4.style.display = "block";
+        des4.style.opacity = 1;
+        btn4.style.opacity = 0;       
+        bhide4.style.opacity = 1;
+        bhide4.style.display = "block";
+    }
+    function hide4 () {
+            des4.style.display = "none";
+            des4.style.opacity = 0;
+            btn4.style.opacity = 1;
+            bhide4.style.opacity = 0;
+            bhide4.style.display = "none";
+    }
+    const btn5 = document.querySelector(".btn5");
+    const des5 = document.querySelector(".des5");
+    const bhide5 = document.querySelector(".hide5");
+    function view5 (){
+        des5.style.display = "block";
+        des5.style.opacity = 1;
+        btn5.style.opacity = 0;       
+        bhide5.style.opacity = 1;
+        bhide5.style.display = "block";
+    }
+    function hide5 () {
+            des5.style.display = "none";
+            des5.style.opacity = 0;
+            btn5.style.opacity = 1;
+            bhide5.style.opacity = 0;
+            bhide5.style.display = "none";
+    }
